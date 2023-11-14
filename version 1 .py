34619be27055e0e455fc2e0afaaf5d62117d2c15
kendiiiiii <150760631+kendiiiiii@users.noreply.github.com>
2023-11-13 22:42:42 -0500

    version 1 .py

--- version 1 .py
+++ version 1 .py
@@ -1,5 +1,23 @@
 import pygame
 
+def show_title_screen():
+    title_running = True
+    while title_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                title_running = False
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                title_running = False
+
+        screen.fill((0, 0, 0))  # Fill the screen with a color
+        font = pygame.font.Font(None, 74)
+        text = font.render("Horror Maze", True, (255, 255, 255))
+        screen.blit(text, (200, 250))  # Adjust the position as needed
+        pygame.display.flip()
+
 # Initialize Pygame
 pygame.init()
 
@@ -11,6 +29,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # Set the title of the window
 pygame.display.set_caption("horror maze")
 
+show_title_screen()
+
 # Game loop
 running = True
 while running:
@@ -18,8 +38,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Update the display serious?
+    # Update the display
     pygame.display.flip()
 
 # Quit Pygame
 pygame.quit()
+
