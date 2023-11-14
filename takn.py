3f5edd7990a69e1b611ddb19ee52fb4da3ed1de3
kendiiiiii <150760631+kendiiiiii@users.noreply.github.com>
2023-11-13 22:18:24 -0500

    idk

--- takn.py
+++ takn.py
@@ -1,16 +1,1 @@
-import pygame
 
-pygame.init()
-
-SCREEN_WIDTH = 8008
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-run = True
-while run:
-    
-    for even in pygame.event.get():
-        run = False
-        
-pygame.quit()
