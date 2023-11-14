5b0be01bca4a86c3c8ba50850a90c561185dfe3f
kendiiiiii <150760631+kendiiiiii@users.noreply.github.com>
2023-11-13 22:00:58 -0500

    Add files via upload

---
+++ version 1 .py
@@ -0,0 +1,25 @@
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+screen_width = 1920
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Set the title of the window
+pygame.display.set_caption("horror maze")
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
